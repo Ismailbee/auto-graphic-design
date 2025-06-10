8e6928381f447574ccb6b5af862b8ca766325623
Suleiman3008 <abdullahikafayatsuleiman@gmail.com>
2025-06-10 10:25:37 -0700

    sideBar

--- src/components/pages/SideBar/sideBar.vue
+++ src/components/pages/SideBar/sideBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div :class="['flex flex-col items-center justify-center h-screen space-y-4 transition-all duration-500', sidebarOpen ? 'w-[330px]' : 'w-[90px]']">
+  <div :class="['flex flex-col items-center justify-center h-screen space-y-4 transition-all duration-500', sidebarOpen ? 'w-[285px]' : 'w-[90px]']">
     
     <!-- Toggle Button (Circle) -->
    <div v-if="!sidebarOpen"
-   class="flex items-center ml-[-80px] w-[150px] h-[150px] rounded-full bg-primary">
+   class="flex items-center ml-[-80px] w-[140px] h-[135px] rounded-full bg-primary">
      <div 
-         class="flex items-center ml-[80px] justify-center tex  t-white transition rounded-full cursor-pointer h-[60px] w-[60px] bg-contrast hover:scale-110"
+         class="flex items-center ml-[80px] justify-center tex  t-white transition rounded-full cursor-pointer h-[50px] w-[50px] bg-contrast hover:scale-110"
          @click="toggleSidebar">
       <ion-icon :icon="add" class="text-3xl text-white transition-transform hover:scale-125" />
     </div>
@@ -15,97 +15,97 @@
     <ion-content v-if="sidebarOpen" class="relative flex flex-col w-full h-screen px-2 pt-6 text-white rounded-b-50">
 
       <!-- Collapse Arrow -->
-      <div class="absolute z-10 flex items-center justify-center w-8 h-8 mt-2 mr-2 bg-transparent rounded-full cursor-pointer top-8 right-7 hover:scale-110 hover:bg-[#BA6900]" 
+      <div class="absolute z-10 flex items-center justify-center w-6 h-6 mt-2 mr-2 bg-transparent rounded-full cursor-pointer top-5 right-3 hover:scale-110 hover:bg-[#BA6900]" 
               @click="toggleSidebar">
-        <ion-icon :icon="chevronBackOutline" class="text-2xl text-white" />
+        <ion-icon :icon="chevronBackOutline" class="text-xl text-white" />
       </div>
 
       <!-- Sidebar Menu Items -->
-      <ion-list class="pt-[80px] rounded-b-30 bg-primary overflow-y-auto h-screen">
+      <ion-list class="pt-[40px] rounded-b-30 bg-primary overflow-y-auto h-screen">
        <div>
-          <ion-item button lines="none" class="group  text-[#BA6900] bg-transparent hover:secondary hover:bg-primary-contrast"
+          <ion-item button lines="none" class="group  text-[#BA6900] bg-transparent hover:secondary  pr-4 pt-4 hover:bg-primary-contrast"
                     style="--background: transparent;  --color: #BA6900;"
                     @click="navigateTo('/home')">
-            <ion-icon :icon="home" slot="start" class="text-2xl pl-5 text-white group-hover:text-[#BA6900]" />
-            <ion-label><strong class="hover:text-white text-[18px]">Home</strong></ion-label>
+            <ion-icon :icon="home" slot="start" class="text-xl pl-5 text-white group-hover:text-[#BA6900] mr-4" />
+            <ion-label><strong class="hover:text-white text-[17px]">Home</strong></ion-label>
           </ion-item>
           
-          <ion-item button lines="none" class="group text-xl text-[#BA6900] hover:bg-primary-contrast"
+          <ion-item button lines="none" class="group text-xl text-[#BA6900]  hover:bg-primary-contrast"
                    style="--background: transparent;  --color: #BA6900;"
                    @click="navigateTo('/myAccountPage')">
-          <ion-icon :icon="personCircle" slot="start" class="text-2xl pl-5 text-white group-hover:text-[#BA6900]" />
-          <ion-label><strong class="hover:text-white text-[18px]">My Account</strong></ion-label>
+          <ion-icon :icon="personCircle" slot="start" class="text-xl pl-5 text-white group-hover:text-[#BA6900] mr-4" />
+          <ion-label><strong class="hover:text-white text-[17px]">My Account</strong></ion-label>
           </ion-item>
         </div>
         
-        <div class="p-2 border-[#794613] border-b-[0.01px] border-t-[0.01px]">
+        <div class="pt-4 pb-4 border-[#794613] border-b-[0.01px] border-t-[0.01px]">
           <ion-item-button1 @click="navigateTo('/scheduling')" class="group">
-              <ion-icon :icon="appsSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
-              <ion-label>Scheduling</ion-label>
+              <ion-icon :icon="appsSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
+              <ion-label class="shrink-label">Scheduling</ion-label>
           </ion-item-button1>
           
           <ion-item-button1 @click="navigateTo('/mockupPage')" class="group">
-              <ion-icon :icon="walletSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
-              <ion-label>Mock-ups</ion-label>
+              <ion-icon :icon="walletSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
+              <ion-label class="shrink-label">Mock-ups</ion-label>
           </ion-item-button1>
           
           <ion-item-button1 @click="navigateTo('/videosPage')" class="group">
-              <ion-icon :icon="logoYoutube" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
-              <ion-label>Videos</ion-label>
+              <ion-icon :icon="logoYoutube" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
+              <ion-label class="shrink-label">Videos</ion-label>
           </ion-item-button1>          
         </div>
         
-        <div class="p-2 border-[#794613] border-b-[0.01px] border-t-[0.01px]">
-          <h3 class="m-0 pl-7 text-[#BA6900]">HELP</h3>
+        <div class="pt-4 pb-4 border-[#794613] border-b-[0.01px] border-t-[0.01px]">
+          <h5 class="m-0 pl-7 text-[#BA6900]">HELP</h5>
           <ion-item-button1 @click="navigateTo('/subscriPage')" class="group">
-              <ion-icon :icon="personAddSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
-              <ion-label >Subscription</ion-label>
+              <ion-icon :icon="personAddSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
+              <ion-label class="shrink-label">Subscription</ion-label>
           </ion-item-button1>
           
           <ion-item-button1 @click="navigateTo('/useredeemcodePage')" class="group">
-              <ion-icon :icon="bagAddSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
+              <ion-icon :icon="bagAddSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
               <ion-label class="shrink-label">Use Redeem Code</ion-label>
           </ion-item-button1>
           
           <ion-item-button1 @click="navigateTo('/suggestfeaturePage')" class="group">
-              <ion-icon :icon="bulbSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
+              <ion-icon :icon="bulbSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
               <ion-label class="shrink-label">Suggest a Feature</ion-label>
           </ion-item-button1>  
                   
           <ion-item-button1 @click="navigateTo('/helpcenterPage')" class="group">
-              <ion-icon :icon="helpCircleSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
+              <ion-icon :icon="helpCircleSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
               <ion-label class="shrink-label">Help Center</ion-label>
           </ion-item-button1>  
                   
           <ion-item-button1 @click="navigateTo('/rateappPage')" class="group">
-              <ion-icon :icon="starHalfSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
-              <ion-label>Rate App</ion-label>
+              <ion-icon :icon="starHalfSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
+              <ion-label class="shrink-label">Rate App</ion-label>
           </ion-item-button1>                    
         </div>
        
-        <div class="p-2 border-[#794613] border-t-[0.01px]">
-          <h3 class="m-0 pl-7 text-[#BA6900]">LEGAL</h3>
+        <div class="pt-4 pb-4 border-[#794613] border-t-[0.01px]">
+          <h5 class="m-0 pl-7 text-[#BA6900]">LEGAL</h5>
           <ion-item-button1 @click="navigateTo('/contactPage')" class="group">
-              <ion-icon :icon="chatbubbleSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
+              <ion-icon :icon="chatbubbleSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
               <ion-label class="text-white shrink-label group-hover:text-[rgb(80,40,0)]">Contact Support</ion-label>
           </ion-item-button1>
           
           <ion-item-button1 @click="navigateTo('/termservicePage')" class="group">
-              <ion-icon :icon="readerSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
+              <ion-icon :icon="readerSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
               <ion-label class="shrink-label">Terms of Service</ion-label>
           </ion-item-button1>
           
           <ion-item-button1 @click="navigateTo('/privacypolicyPage')" class="group">
-              <ion-icon :icon="shieldHalfSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
+              <ion-icon :icon="shieldHalfSharp" slot="start" class="text-[14px] text-white mr-5 group-hover:text-[#502800] mr-5" />
               <ion-label class="shrink-label">Privacy Policy</ion-label>
           </ion-item-button1>  
                   
           <ion-item-button1 @click="navigateTo('/privacysettingPage')" class="group">
-              <ion-icon :icon="shieldCheckmarkSharp" slot="start" class="text-xl text-white group-hover:text-[#502800] " />
+              <ion-icon :icon="shieldCheckmarkSharp" slot="start" class="text-[14px] text-white group-hover:text-[#502800] mr-5" />
               <ion-label class="shrink-label">Privacy Settings</ion-label>
           </ion-item-button1>                      
         </div>
-        
+         
       </ion-list>
 
     </ion-content>
@@ -168,15 +168,12 @@ ion-item:hover {
   --background: #BA6900;
   --color: #502800;
 }
-ion-label {
-  font-size: 0.8rem;
-  font-weight: 300;
-}
 .shrink-label {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   display: block;
+  font-size: 13px;
 }
 
 </style>
--- src/components/pages/ionElement/ion-item-button1.vue
+++ src/components/pages/ionElement/ion-item-button1.vue
@@ -1,5 +1,5 @@
 <template>
-    <ion-item button lines="none" class="flex items-center justify-center h-[2px] p-4 text-white">
+    <ion-item button lines="none" class="flex items-center justify-center h-[2px] pl-4 pr-4 pt-3 pb-3 text-white">
           <slot></slot>
     </ion-item>
 </template>
